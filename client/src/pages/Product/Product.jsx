7fa2a0b3ba227da65cdd45377807be5405abfc09
Noor Chasib <noorchasib@gmail.com>
2023-03-06 21:14:16 -0800

    setup redux components and persistence

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,16 +1,20 @@
-import { React, useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartReducer";
 
 const Product = () => {
 	const id = useParams().id;
-	const [selectedImg, setSelectedImg] = useState("image");
+	const [selectedImg, setSelectedImg] = useState("img");
 	const [quantity, setQuantity] = useState(1);
 
+	const dispatch = useDispatch();
 	const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
 	return (
@@ -63,23 +67,34 @@ const Product = () => {
 							{quantity}
 							<button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
 						</div>
-						<button className="add">
-							<AddShoppingCartIcon />
-							ADD TO CART
+						<button
+							className="add"
+							onClick={() =>
+								dispatch(
+									addToCart({
+										id: data.id,
+										title: data.attributes.title,
+										desc: data.attributes.desc,
+										price: data.attributes.price,
+										img: data.attributes.img.data.attributes.url,
+										quantity,
+									})
+								)
+							}
+						>
+							<AddShoppingCartIcon /> ADD TO CART
 						</button>
 						<div className="links">
 							<div className="item">
-								<FavoriteBorderIcon />
-								ADD TO WISH LIST
+								<FavoriteBorderIcon /> ADD TO WISH LIST
 							</div>
 							<div className="item">
-								<BalanceIcon />
-								ADD TO COMPARE
+								<BalanceIcon /> ADD TO COMPARE
 							</div>
 						</div>
 						<div className="info">
 							<span>Vendor: Polo</span>
-							<span> Product Type: T-Shirt</span>
+							<span>Product Type: T-Shirt</span>
 							<span>Tag: T-Shirt, Women, Top</span>
 						</div>
 						<hr />
